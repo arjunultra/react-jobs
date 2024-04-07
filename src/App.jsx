f5bd9995f4677d4bbab981f4f54482fa7ab5f3ee
Arjun Arasu <arjunbalan1994@gmail.com>
2024-04-07 12:22:20 +0530

    useState started

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
-
+import ViewAllJobs from "./components/ViewAllJobs";
 function App() {
   // Write Javascript code above return
 
@@ -15,14 +15,8 @@ function App() {
       <HomeCards />
       {/* <!-- Browse Jobs --> */}
       <JobListings />
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      {/* Button Component */}
+      <ViewAllJobs />
     </>
   );
 }
